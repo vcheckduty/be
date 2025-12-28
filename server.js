3a6d5eb30f6040a85f535d8adae9086c756132b5
dduongCongSon <sonduongcong8@gmail.com>
2025-12-28 22:59:30 +0700

    fix: Add PATCH method to CORS in server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const corsOptions = {
     process.env.FRONTEND_URL,
   ].filter(Boolean),
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
